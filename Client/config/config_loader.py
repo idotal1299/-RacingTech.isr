c693800395f21fd3928f3bb13bf0c398e8dca5b8
idotal1299 <73936903+idotal1299@users.noreply.github.com>
2025-06-11 13:26:17 +0300

    Remove Client/config directory from tracking

--- Client/config/config_loader.py
+++
@@ -1,7 +0,0 @@
-import json
-import os
-
-def load_config():
-    path = os.path.join(os.path.dirname(__file__), 'config.json')
-    with open(os.path.abspath(path), "r") as f:
-        return json.load(f)
